254bfff93f825dd77191ada070b48aa04c8c60d3
Xavier Morera <xavier@familiamorera.com>
2023-07-03 22:11:01 -0600

    mod 3 (+2 squashed commit)
    Squashed commit:
    
    [c4c09e6] Mod 2
    
    [4852661] Mod 1

---
+++ the_modules.py
@@ -0,0 +1,15 @@
+import os 
+import shutil
+# Install psutil using pip install psutil
+import psutil
+
+print('- os:')
+print(dir(os))
+
+print('- shutil:')
+print(dir(shutil))
+
+print('- psutil:')
+print(dir(psutil))
+print()
+ 
